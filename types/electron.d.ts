9fe7c3ec18e7febddf46a0fe66e28bbd7c92f884
Anees <aneespengad4447@gmail.com>
2026-09-01 16:13:25 +0530

    feat(electron): add TypeScript definitions for electronAPI interface

---
+++ types/electron.d.ts
@@ -0,0 +1,11 @@
+export {};
+
+declare global {
+  interface Window {
+    electronAPI: {
+      platform: string;
+      isElectron: boolean;
+      getAppVersion: () => string | undefined;
+    };
+  }
+}
