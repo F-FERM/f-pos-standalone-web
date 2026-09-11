001dec90845cb6f83c65160a4f37e1e6834ebf12
Anees <aneespengad4447@gmail.com>
2026-09-11 11:35:02 +0530

    fix: update axios base URL to use localhost for API requests

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -17,7 +17,7 @@ const processQueue = (error: any, token: string | null = null) => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001/api/",
+  baseURL:"http://127.0.0.1:3010/api/",
   headers: {
     "Content-Type": "application/json",
   },
