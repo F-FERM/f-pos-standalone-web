3ebc8f722541fa7d6d5fa8e7be16927b2f07c40d
Anees <aneespengad4447@gmail.com>
2026-09-16 11:13:54 +0530

    feat(home): add close app functionality to close button
    - import closeApp helper from electron library
    - attach closeApp handler to the close button click event

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -21,6 +21,7 @@ import saleIcon from "../../../public/images/icons/sale.png";
 import settingsIcon from "../../../public/images/icons/settings.png";
 import userIcon from "../../../public/images/icons/user.png";
 import logo from "../../../public/images/login/fposlogo.png";
+import { closeApp } from "@/src/lib/electron";
 
 interface MenuItem {
   label: string;
@@ -93,7 +94,13 @@ export default function RestaurantDashboard() {
             <a href="#" className="hover:opacity-80">ABOUT</a>
             <a href="#" className="hover:opacity-80">SUPPORT</a>
           </nav>
-          <button aria-label="Close" className="text-black hover:opacity-80">
+
+          <button
+            type="button"
+            aria-label="Close"
+            onClick={closeApp}
+            className="text-black hover:opacity-80"
+          >
             <X className="h-5 w-5 sm:h-6 sm:w-6" />
           </button>
         </div>
